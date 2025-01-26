9cf1f2b12758a6ae1152953297992095a4bb76fe
Fawad Jawed <154456257+Fawad-Jawed@users.noreply.github.com>
2025-01-26 13:26:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const beneficiaryRoutes = require('./routes/beneficiaryRoutes');
+// const tokenRoutes = require('./routes/tokenRoutes');
 const cors = require('cors');
 const { authenticate, authorize } = require('./middleware/authMiddleware');
 const { editBeneficiary, deleteBeneficiary } = require('./controllers/beneficiaryController');
@@ -16,22 +17,14 @@ connectDB();
 
 const app = express();
 
-// CORS options for strict control
 const corsOptions = {
     origin: 'https://hackathon-frontend-blush.vercel.app',  // Your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // If you need to send cookies, set this to true
 };
 
-// Enable CORS with the above options
-app.use(cors(corsOptions));
-
-// Handle preflight OPTIONS requests
-app.options('*', cors(corsOptions));  // Add this line to handle preflight requests
 
 // Middleware
-app.use(express.json());  // Parse JSON requests
+app.use(cors(corsOptions)); // Enable CORS for all routes
+app.use(express.json()); // Parse JSON requests
 
 // API Routes
 app.use('/api/auth', authRoutes);
@@ -40,11 +33,9 @@ app.use('/api/beneficiaries', beneficiaryRoutes);
 // Protected Routes
 app.put('/api/beneficiaries/editBeneficiary', authenticate, authorize(['Admin', 'Receptionist']), editBeneficiary);
 app.delete('/api/beneficiaries/deleteBeneficiary', authenticate, authorize(['Admin']), deleteBeneficiary);
+// app.use('/api/tokens', tokenRoutes);
 
-// Admin routes
 app.get('/api/auth/getAllUsers', authenticate, authorize(['Admin']), getAllUsers);
-app.put('/api/auth/editUser', authenticate, authorize(['Admin']), editUser);
-app.delete('/api/auth/deleteUser', authenticate, authorize(['Admin']), deleteUser);
 
 // Default route
 app.get('/', (req, res) => {
